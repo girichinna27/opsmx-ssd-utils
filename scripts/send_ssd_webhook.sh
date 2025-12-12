d64bbbccf4b73e8ede271e912c1a41f33371f0bf
giribabuthanikonda <52537095+girichinna27@users.noreply.github.com>
2025-12-12 13:23:55 +0530

    Update send_ssd_webhook.sh

--- scripts/send_ssd_webhook.sh
+++ scripts/send_ssd_webhook.sh
@@ -10,12 +10,18 @@ curl --location "$SSD_URL/webhook/v1/ssd" \
     \"jobname\": \"${JOB_NAME}\",
     \"buildnumber\": \"${BUILD_NUMBER}\",
     \"joburl\": \"${JOB_URL}\",
-    \"builduser\": \"ssdadmin@opsmx.io\",
+    \"builduser\": \"isdadmin@opsmx.io\",
     \"giturl\": \"${GIT_URL}\",
-    \"gitbranch\": \"${BRANCH_NAME}\",
+    \"gitbranch\": \"${BRANCH_NAME}\",    
+    \"account\": \"isd-spin\",
+    \"applicationname\": \"isd-release\",
+    \"deployedby\": \"isdadmin@opsmx.io\",
+    \"artifactTags\": \"${TAG}\",
+    \"namespace\": \"isd-release\",
     \"artifacts\": [
-      { \"image\": \"${IMAGENAME}\" }
+      { \"image\": \"${IMAGENAME}\",
+        \"service\": \"${JOB_NAME}\"
+      }      
     ]
   }"
-
 echo "SSD Webhook sent successfully."
